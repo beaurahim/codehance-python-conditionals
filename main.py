9630f0835325072746b3cefc4ba9e81f58ec4595
dizzysufi <>
2021-05-29 19:48:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+#x = 15
+#y = 7
+
+#if x > y:
+#  print('yes')
+
+
+#x = 15
+#y = 7
+#if 'hello' in 'hello world':
+  #print('yes')
+
+  #=======
+
+#x = 7
+#y = 7
+
+#if x > y:
+#      #print('yes it is greater')
+#elif x < y:
+#      print('no it is less')
+#else:
+##     print('neither')
+
+#''''
+#nested if statements
+#'''
+
+#num = 3
+#if num >= 0:
+ # if num == 0:
+  #  print("zero")
+  #else:
+#   print("postive number")
+#else:
+#  print('negative number')
+#
+#  '''
+#  ##
+
+#''''
+#one-line if statements
+#'''
+
+#if 7 >5: print('yes')
+
+#'''
+#ternary operator
+#'''
+#print('yes') if 7 < 5 else print('yes')
+
+#if 7>5:
+#  print('yes')
+#  else('no')
+
